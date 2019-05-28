467ca9f68daf4bc5cd5d695e39bc78c66effe1a0
Daniela <dgaytan@nyx.mx>
2019-05-27 20:08:16 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,27 @@
 // ===========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ===========================
+// ========= Entonrno ========
+// ===========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===========================
+// ===== Base de Datos =======
+// ===========================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+
+    urlDB = 'mongodb+srv://danielaNyx:DNceSiY9DpXcBDc@cluster0-ydioj.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB
